46249476004f126d3c4fa652f68d822f30d4bd83
MasonJ-A <azzoparm@mcmaster.ca>
2025-08-09 17:09:01 -0400

    did a little more

--- com/craftinginterpreters/lox/lox.java
+++ com/craftinginterpreters/lox/lox.java
@@ -10,28 +10,32 @@ import java.util.List;
 import java.util.Scanner;
 
 public class lox{
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException{
-        if (args.length > 1){
+        if (args.length > 1){ //if there is more than one argument exit
             System.out.println("Usage: jlox [script]");
             System.exit(64);
-        }else if (args.length == 1) {
+        }else if (args.length == 1) {//if there is one argument runFile with the argument as the path
             runFile(args[0]);
-        } else {
+        } else {//otherwise runPrompt
             runPrompt();
         }
     }
     private static void runFile(String path) throws IOException{
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
+        byte[] bytes = Files.readAllBytes(Paths.get(path));//turns path into bytes
+        run(new String(bytes, Charset.defaultCharset()));//turns bytes into a String and inputs it as an argument for run()
+
+        if (hadError) System.exit(65);
     }
     private static void runPrompt() throws IOException{
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
-        for(;;){
+        InputStreamReader input = new InputStreamReader(System.in);//creates a Input stream reader
+        BufferedReader reader = new BufferedReader(input);//creates a buffered reader using the input stream reader
+        for(;;){//loops until break
             System.out.print(">");
-            String line = reader.readLine();
-            if (line == null)break;
-            run(line);
+            String line = reader.readLine();//prompts the user for an input
+            if (line == null)break;//breaks if no input
+            run(line);//uses the input as an argument for run
+            hadError = false;
         }
     }
     private static void run(String source){
@@ -41,5 +45,13 @@ public class lox{
         for (Token token : tokens){
             System.out.println(token);
         }
+    
+    }
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+    private static void report(int line, String where, String message){
+        System.err.println("[line"+ line + "] Error" +": " + message);
+        hadError = True
     }
 }
