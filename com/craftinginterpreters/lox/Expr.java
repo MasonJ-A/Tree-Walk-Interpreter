7e1225d06c97780bdfeda8689caa4729f9499556
MasonJ-A <azzoparm@mcmaster.ca>
2025-08-23 13:54:11 -0400

    Revert "created Expr class and GenerateAst Tool"
    This reverts commit d0780cb329e5e885797ea4e4a55067c1cea53ead.

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -1,14 +1,26 @@
 package com.craftinginterpreters.lox;
+/* EXPRESSION SYNTAX
+A capitalized terminal represents a single lexeme whose representation may vary
+A quated string represents terminals that match exact lexemes
 
-import java.util.List;
+expression     → literal | unary | binary | grouping ;
 
+literal        → NUMBER | STRING | "true" | "false" | "nil" ;
+grouping       → "(" expression ")" ;
+unary          → ( "-" | "!" ) expression ;
+binary         → expression operator expression ;
+operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
+               | "+"  | "-"  | "*" | "/" ; */
 abstract class Expr {
-  interface Visitor<R> {
-    R visitBinaryExpr(Binary expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitUnaryExpr(Unary expr);
-  }
-
-  abstract <R> R accept(Visitor<R> visitor);
+    static class Binary extends Expr{
+        Binary(Expr left, Token operator, Expr right){
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+    
 }
