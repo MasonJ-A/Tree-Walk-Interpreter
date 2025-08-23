d0780cb329e5e885797ea4e4a55067c1cea53ead
MasonJ-A <azzoparm@mcmaster.ca>
2025-08-23 13:15:40 -0400

    created Expr class and GenerateAst Tool

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -1,26 +1,40 @@
 package com.craftinginterpreters.lox;
-/* EXPRESSION SYNTAX
-A capitalized terminal represents a single lexeme whose representation may vary
-A quated string represents terminals that match exact lexemes
 
-expression     → literal | unary | binary | grouping ;
+import java.util.List;
 
-literal        → NUMBER | STRING | "true" | "false" | "nil" ;
-grouping       → "(" expression ")" ;
-unary          → ( "-" | "!" ) expression ;
-binary         → expression operator expression ;
-operator       → "==" | "!=" | "<" | "<=" | ">" | ">="
-               | "+"  | "-"  | "*" | "/" ; */
 abstract class Expr {
-    static class Binary extends Expr{
-        Binary(Expr left, Token operator, Expr right){
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
-        final Expr left;
-        final Token operator;
-        final Expr right;
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
     }
-    
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    final Expr expression;
+  }
+  static class Literal extends Expr {
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    final Object value;
+  }
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Token operator;
+    final Expr right;
+  }
 }
